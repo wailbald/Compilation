44eeb6e85de057ec94efd9e165635ac266ac2876
Ennaox <alexis.laplanche@hotmail.fr>
2022-12-29 00:11:38 +0100

    Ajout du Cmake + ajout des regex

--- Makefile
+++
@@ -1,9 +0,0 @@
-all: compil
-
-compil:
-
-run_test: test_token
-	./test
-
-test_token: src/test_token.cpp src/token.cpp
-	g++ $^ -o $@
---
+++ src/lexer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+const std::string line_terminator(";{");
+
+std::string read_line(FILE* f)
+{
+	std::string line;
+	char buff=1;
+	while()
+}
