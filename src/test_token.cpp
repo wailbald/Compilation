c068355c936f2da56b80ce129a390de2eba5d7a0
wailbald <nicodias92@gmail.com>
2022-12-28 15:28:04 +0100

    début des fonctions permettant de ranger les tokens dans un arbre, et changement dans les fichiers token.cpp et test_token.cpp pour des tests

--- src/test_token.cpp
+++ src/test_token.cpp
@@ -1,4 +1,4 @@
-#include "token.hpp"
+#include "ast.hpp"
 #include <vector>
 
 int main()
@@ -10,11 +10,15 @@ int main()
 	tok.push_back(Token(INT,(location){1,4,4},"1"));
 	tok.push_back(Token(SEMICOLON,(location){1,5,5}));
 
+	std::cout<<"et ici"<<tok[1]<<std::endl;
+
 	for(size_t i = 0; i<tok.size(); i++)
 	{
 		std::cout<<tok[i];
 	}
 	std::cout<<std::endl;
+
+	create_assign(tok,0);
 
 	return 0;
 }
--- src/token.cpp
+++ src/token.cpp
@@ -4,4 +4,3 @@ std::ostream& operator<<(std::ostream& os, Token& tok)
 {
 	return os<<"\\@"<<token_name[tok.type]<<","<<tok.text<<"\\";
 }
-
