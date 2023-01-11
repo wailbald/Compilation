9e1985ee5235558a2c13364fcce9c075384cae1a
Ennaox <alexis.laplanche@hotmail.fr>
2023-01-12 00:29:02 +0100

    Ajout des classes de la nouvelle structure de l'arbre

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,5 +1,6 @@
 #include "token.hpp"
 #include <vector>
+#include <cstdint>
 
 class Node
 {
@@ -30,3 +31,274 @@ class Tree
 int assign_check_type(Token t);
 int verif_assign(std::vector<Token> tok, int i);
 Tree create_assign(std::vector<Token> tok, int i);
+
+
+typedef enum { t_undef = 0, t_int, t_string, t_void, t_tab } Type;
+static std::string type_name[] = {"t_undef","int","string","void","tab"};
+
+typedef enum { o_plus, o_minus, o_div, o_mult, o_module, o_lshift, o_rshift, o_and, o_or, o_xor, o_comp, c_eq, c_neq, c_lt, c_gt, c_le, c_ge, c_and, c_or} Operation;
+static std::string operation_name[] = { "+", "-", "/", "*", "%", "<<", ">>", "&", "|", "^", "~", "==", "!=", "<", ">", "<=", ">=", "&&", "||"};
+
+class visitor{};
+
+class NewNode{	
+public:
+	location loc;
+
+	NewNode(location &loc_) : loc(loc_){};
+
+	void accept(visitor v);
+};
+
+class Expr: public NewNode {
+public:
+	Expr(location &loc_) : NewNode(loc_) {};
+};
+
+class Decl: public Expr {
+public:
+	std::string name;
+	Type type;
+	int depth = -1;
+
+	Decl(location &loc_,std::string &_name,Type &_type, int depth_ = -1)
+      : Expr(loc_), name(_name), type(_type), depth(depth_) {};
+
+    void set_depth(int _depth) {
+    	if(depth == -1 && _depth != -1)
+      		depth = _depth;
+  	};
+
+  	int get_depth(){return depth;};
+};
+
+class IntegerLiteral : public Expr {
+public:
+	int32_t value;
+
+	IntegerLiteral(location &loc_,int32_t &_value)
+      : Expr(loc_), value(_value) {};
+
+};
+
+class StringLiteral : public Expr{
+public:
+	std::string value;
+
+	StringLiteral(location &loc_,std::string &value_)
+      : Expr(loc_), value(value_) {};
+
+};
+
+class BinaryOperator : public Expr {
+  Expr *left;
+  Expr *right;
+
+public:
+  Operation op;
+
+  // Constructor
+  BinaryOperator(location &_loc, Expr *_left, Expr *_right,Operation &_op)
+      : Expr(_loc), left(_left), right(_right), op(_op) {};
+
+  // Destructor
+  virtual ~BinaryOperator() {
+    delete right;
+    delete left;
+  };
+
+  Expr &get_left(){return *left;};
+  Expr &get_right(){return *right;};
+};
+
+  class Sequence : public Expr{
+  	std::vector<Expr *> exprs;
+
+  public:
+  	Sequence(location &_loc, std::vector<Expr *> &_exprs)
+      : Expr(_loc), exprs(_exprs) {};
+
+  	virtual ~Sequence() {
+    for (auto expr : exprs)
+      delete expr;
+  	}
+
+  	std::vector<Expr *> &get_exprs() { return exprs; };
+  };
+
+class Identifier : public Expr {
+	Decl *decl = nullptr;
+  	int depth = -1;
+
+public:
+  	std::string name;
+
+	Identifier(location &_loc, std::string &_name)
+  		: Expr(_loc), name(_name) {};
+
+  	void set_decl(Decl *_decl){
+  		if(!decl && _decl)
+  			decl = _decl;
+  	};
+
+  	Decl* get_decl(){return decl;};
+
+  	void set_depth(int _depth) {
+    	if(depth == -1 && _depth != -1)
+      	depth = _depth;
+  	}
+  	int get_depth(){return depth;};
+};
+
+class IfThenElse : public Expr{
+	Expr * condition;
+	Expr * then_part;
+	Expr * else_part;
+
+public:
+	IfThenElse(location &_loc, Expr *_condition, Expr *_then_part,Expr *_else_part)
+      : Expr(_loc), condition(_condition), then_part(_then_part), else_part(_else_part) {}
+
+    virtual ~IfThenElse() {
+	    delete else_part;
+	    delete then_part;
+	    delete condition;
+  	};
+
+  	Expr &get_condition(){return *condition;};
+  	Expr &get_then(){return *then_part;};
+  	Expr &get_else(){return *else_part;};
+};
+
+class VarDecl : public Decl {
+	Expr *expr;
+	bool escapes = false;
+
+public:
+	VarDecl(location &_loc, std::string &_name, Type &_type_name, Expr *_expr)
+	: Decl(_loc, _name, _type_name), expr(_expr){};
+
+	virtual ~VarDecl() { delete expr; };
+	Expr &get_expr()
+	{
+		return *expr;
+	}
+
+	void set_escapes(){escapes = true;};
+	bool get_escapes(){return escapes;};
+};
+
+class FunDecl : public Decl {
+	std::vector<VarDecl *> params;
+	Expr *expr;
+	std::string name;
+
+public:
+	FunDecl(location &_loc, std::string &_name, Type &_type_name, std::vector<VarDecl *> &_params, Expr *_expr)
+	: Decl(_loc, _name, _type_name), params(_params), expr(_expr) {};
+
+	virtual ~FunDecl() {
+	    delete expr;
+	    for (auto param : params)
+	      delete param;
+  	}
+
+  	std::vector<VarDecl *> &get_params(){return params;};
+  	Expr &get_expr(){return *expr;};
+
+};
+
+class FunCall : public Expr {
+	std::vector<Expr *> args;
+	FunDecl *decl = nullptr;
+
+public:
+	std::string func_name;
+
+	FunCall(location &_loc, std::vector<Expr *> &_args, std::string &_func_name)
+	: Expr(_loc), args(_args), func_name(_func_name) {};
+
+	virtual ~FunCall() {
+	    for (auto arg : args)
+	      delete arg;
+  	}
+
+  	std::vector<Expr *> &get_args(){return args;};
+
+  	void set_decl(FunDecl *_decl){
+  		if(!decl && _decl)
+  			decl = _decl;
+  	};
+
+  	FunDecl &get_decl(){return *decl;};
+};
+
+class Loop : public Expr{
+public:
+	Loop(location &_loc) : Expr(_loc) {};
+};
+
+class WhileLoop : public Loop{
+	Expr *condition;
+	Expr *body;
+
+public:
+	WhileLoop(location &_loc, Expr *_condition, Expr *_body)
+	: Loop(_loc), condition(_condition), body(_body) {};
+
+	virtual ~WhileLoop() {
+    	delete body;
+    	delete condition;
+  	};
+
+  	Expr &get_condition(){return *condition;};
+  	Expr &get_body(){return *body;};
+};
+
+class ForLoop : public Loop {
+	VarDecl *variable;
+	Expr *high;
+	Expr *body;
+
+public:
+	ForLoop(location &_loc, VarDecl *_variable, Expr *_high, Expr *_body)
+      : Loop(_loc), variable(_variable), high(_high), body(_body) {};
+
+    virtual ~ForLoop() {
+	    delete body;
+	    delete high;
+	    delete variable;
+  	}
+
+  	VarDecl &get_variable() {return *variable;};
+  	Expr &get_high() {return *high;};
+  	Expr &get_body(){return *body;};
+};
+
+class Break : public Expr {
+	Loop * loop = nullptr;
+public:
+	Break(location &_loc) : Expr(_loc){};
+	void set_loop(Loop *_loop){
+		if(!loop && _loop)
+			loop = _loop;
+	}
+
+	Loop &get_loop(){return *loop;}
+};
+
+class Assign : public Expr{
+	Identifier *lhs;
+	Expr *rhs;
+
+	Assign(location &_loc, Identifier *_lhs, Expr *_rhs)
+	: Expr(_loc), lhs(_lhs), rhs(_rhs) {};
+
+	virtual ~Assign() {
+	    delete rhs;
+	    delete lhs;
+  	};
+
+  	Identifier &get_lhs(){return *lhs;};
+  	Expr &get_rhs(){return *rhs;};
+};
