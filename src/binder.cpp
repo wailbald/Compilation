d485dfe6371d3170e4f980664d7ff3c47e24c3b2
wailbald <nicodias92@gmail.com>
2023-02-09 15:13:18 +0100

    ajout debut binder

--- src/binder.cpp
+++ src/binder.cpp
@@ -28,6 +28,11 @@ void verif(Decl decl)
 	s[decl.name] = &decl;
 }
 
+Binder::Binder()
+{
+	scope_push();
+}
+
 Decl cherche(std::string name)
 {
 	for(auto s = scopes.crbegin(); s != scopes.crend(); s++)
@@ -40,4 +45,172 @@ Decl cherche(std::string name)
 	}
 	std::cout << "function " << name << std::endl;
 	exit(6);
+}
+
+void visit(IntegerLiteral &i)
+{
+
+}
+
+void visit(StringLiteral &str)
+{
+
+}
+
+void visit(DoubleLiteral &dou)
+{
+
+}
+
+void visit(UnaryOperator &un)
+{
+	un.get_expr()->accept(*this);
+}
+
+void visit(BinaryOperator &bop)
+{
+	bop.get_left()->accept(*this);
+	bop.get_right()->accept(*this);
+}
+
+void visit(Sequence &seq)
+{
+	for(auto iterator = seq.exprs.begin(); iterator != seq.exprs.end(); ++iterator)
+	{
+    	iterator->accept(*this);
+	}
+}
+
+void visit(Identifier &id)
+{
+	VarDecl *var = dynamic_cast<VarDecl*>(&find(id.loc, id.name));
+	if(!var)
+	{
+		std::cout << "erreur la variable n'est pas declaree dans ce scope ou a ete define deux fois" << std::endl;
+		exit(9);
+	}
+	else
+	{
+		id.set_decl(var);
+		id.set_depth(depth);
+	}
+
+	if(id->get_depth() != var.get_depth())
+	{
+		var->set_escape();
+	}
+}
+
+void visit(IfThenElse &ite)
+{
+	scope_push();
+	depth++;
+
+	ite.get_condition()->accept(*this);
+	ite.get_then_part()->accept(*this);
+	ite.get_else_part()->accept(*this);
+
+	scope_pop();
+	depth--;
+}
+
+void visit(VarDecl &var)
+{
+	if(var.get_expr())
+	{
+		var.get_expr()->accept(*this);
+	}
+	verif(var);
+	var.det_depth(depth)
+}
+
+void visit(FunDecl &f_decl)
+{
+	function.push_back(&f_decl);
+	scope_push();
+	depth++;
+
+	for(auto p : f_decl.get_params())
+	{
+		p->accept(*this);
+	}
+	f_decl.get_expr()->accept(*this);
+
+	scope_pop();
+	depth--;
+	function.pop_back();
+}
+
+void visit(FunCall &f_call)
+{
+	FunDecl &f_decl = dynamic_cast<FunDecl&>(find(f_call.loc, f_call.func_name));
+	f_call.set_decl(&f_decl);
+	f_call.set_depth(depth);
+
+	for(auto arg : f_call.get_args())
+	{
+		arg->accept(*this);
+	}
+}
+
+void visit(WhileLoop &wloop)
+{
+	scope_push();
+	depth++;
+
+	wloop.get_condition().accept(*this);
+	loop.push_back(wloop);
+	wloop.get_body().accept(*this);
+	loop.pop_back()
+
+	scope_pop();
+	depth--;
+}
+
+void visit(ForLoop &floop)
+{
+	scope_push();
+	depth++;
+
+	floop.get_variable().accept(*this);
+	floop.get_high().accept(*this);
+	loop.push(back(floop));
+	floop.get_body().accept(*this);
+	loop.pop_back();
+
+	scope_pop();
+	depth--;
+}
+
+void visit(Break &br)
+{
+	if(loop.empty())
+	{
+		std::cout << "Erreur le break est hors d'un if ou d'une boucle" << std::endl;
+		exit(10);
+	}
+
+	br.set_loop(loop.back());
+}
+
+void visit(Assign &ass)
+{
+	ass.get_lhs().accept(*this);
+	ass.get_rhs().accept(*this);
+
+	if(ass.get_lhs().get_decl()->read_only)
+	{
+		std::cout << "Erreur assignation a une variable de lecture" <<std::endl;
+		exit(11);
+	}
+}
+
+void visit(Return &ret)
+{
+	if(function.empty())
+	{
+		std::cout << "Erreur Le return se trouve hors d'une fonction" << std::endl;
+		exit(12);
+	}
+	ret.set_func(function.back());
 }
--- src/binder.hpp
+++ src/binder.hpp
@@ -1,12 +1,37 @@
+#pragma once
 #include <unordered_map>
 #include "ast.hpp"
 
 typedef std::unordered_map<std::string, Decl*> scope;
-std::vector<scope> scopes;
-std::vector<FunDecl> function;
-std::vector<Loop> loop;
-void scope_push();
-void scope_pop();
-scope actuel();
-void verif(Decl decl);
-Decl cherche(std::string);
+
+class Binder{
+
+	std::vector<scope> scopes;
+	std::vector<FunDecl> function;
+	std::vector<Loop> loop;
+	int depth = 0;
+	void scope_push();
+	void scope_pop();
+	scope actuel();
+	void verif(Decl decl);
+	Decl cherche(std::string);
+
+public:
+	Binder();
+	void visit(IntegerLiteral &i);
+	void visit(StringLiteral &str);
+	void visit(DoubleLiteral &dou);
+	void visit(UnaryOperator &un);
+	void visit(BinaryOperator &bop);
+	void visit(Sequence &seq);
+	void visit(Identifier &id);
+	void visit(IfThenElse &ite);
+	void visit(VarDecl &var);
+	void visit(FunDecl &f_decl);
+	void visit(FunCall &f_call);
+	void visit(WhileLoop &wloop);
+	void visit(ForLoop &floop);
+	void visit(Break &br);
+	void visit(Assign &ass);
+	void visit(Return &ret);
+};
